c9f349e8aca5917408714e686f5f7a4359f1c823
ReEkaF <andreeka852@gmail.com>
2023-11-11 12:30:48 +0700

    homepage,connect login register

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="asset/style copy.css">
+</head>
+<body>
+    <div class="headerhome">
+        <img src="asset/pict/misminit.jpg" alt="">
+        <div class="lognregbox">
+            <a href="login.php">Login</a>
+            <a href="register.php">Registrasi</a>
+        </div>
+    </div>
+    <div class="home">
+        <div class="box-home">
+            <h1 class="text-1"> NATURAL GARDEN</h1>
+        </div>
+        <div class="box-1">
+            <p> Kami menyediakan berbagai jenis tanaman dalam pot untuk mempercantik rumah anda</p>
+        </div>
+    </div>
+    <div class="teamhome">
+        <h2>ABOUT US</h2>
+        <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, quasi.</h2>
+    </div>
+    <div class="anoconthome-1">
+        <div class="anoconthome-2">
+            <h2>Lorem, ipsum.</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem maiores, quas, eum dolores aut beatae vero officia eos dicta nulla quaerat quibusdam fugiat corporis asperiores necessitatibus sapiente explicabo eligendi delectus laborum! Veritatis explicabo odit sed. Nobis inventore ut dolore deserunt excepturi voluptates dolorem placeat optio minus, similique ad libero, distinctio iste? Harum ipsa mollitia, error magnam incidunt nihil, doloribus cupiditate adipisci porro necessitatibus veniam doloremque optio animi eaque, odio quae hic minima? Facere rerum qui nam atque fuga rem consectetur voluptate molestias vel nostrum, provident accusamus voluptatem ex eligendi vero nisi totam obcaecati exercitationem! Vero, nulla animi? Laborum, nihil perferendis.</p>
+        </div>
+    </div>
+</body>
+</html>
