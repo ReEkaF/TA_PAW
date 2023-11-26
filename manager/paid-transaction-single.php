43498ecfbb808edeba99ce09c131e6fd8d6d98d4
Graell29 <umuchtar0@gmail.com>
2023-11-26 23:49:08 +0700

    +penjelasan

--- manager/paid-transaction-single.php
+++ manager/paid-transaction-single.php
@@ -5,132 +5,112 @@ $title = 'Paid Transaction Single';
 
 require_once('layouts/header.php');
 
-$connect=mysqli_connect('localhost','root','','store');
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=store', 'root', '');
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die('Connection failed: ' . $e->getMessage());
+}
 
 $transid = isset($_GET['transid']) ? $_GET['transid'] : null;
 
-$dateorder = mysqli_query($connect, "SELECT order_date FROM order_details ordet, orders ord WHERE ord.order_id = $transid");
+$dateorderstmt = $pdo->prepare("SELECT order_date FROM order_details ordet, orders ord WHERE ord.order_id = ?");
+$dateorderstmt->execute([$transid]);
+$dateorder = $dateorderstmt->fetch(PDO::FETCH_ASSOC)['order_date'];
 
-$plantname = mysqli_query($connect, "SELECT plant_name FROM order_details ordet, plants pl WHERE ordet.plant_id = pl.plant_id AND order_id = $transid ORDER BY order_detail_unit_price");
-$plantprice = mysqli_query($connect, "SELECT plant_price FROM order_details ordet, plants pl WHERE ordet.plant_id = pl.plant_id AND order_id = $transid ORDER BY order_detail_unit_price");
-$plantquantity = mysqli_query($connect, "SELECT order_detail_qty FROM order_details WHERE order_id = $transid ORDER BY order_detail_unit_price");
-$planttotal = mysqli_query($connect, "SELECT order_detail_unit_price FROM order_details WHERE order_id = $transid ORDER BY order_detail_unit_price");
-
-$plantquantitytotal = mysqli_query($connect, "SELECT SUM(order_detail_qty) AS quantity FROM order_details WHERE order_id = $transid ORDER BY order_detail_unit_price");
-$planttotalall = mysqli_query($connect, "SELECT SUM(order_detail_unit_price) AS prices FROM order_details WHERE order_id = $transid ORDER BY order_detail_unit_price");
+$plantstmt = $pdo->prepare("SELECT plant_name, plant_price, order_detail_qty, order_detail_unit_price FROM order_details ordet, plants pl WHERE ordet.plant_id = pl.plant_id AND order_id = ? ORDER BY order_detail_unit_price");
+$plantstmt->execute([$transid]);
 
 $plantquantityarray = array();
-while ($row = mysqli_fetch_array($plantquantity)) {
-    array_push($plantquantityarray, $row['order_detail_qty']);
-}
 $planttotalarray = array();
-while ($row = mysqli_fetch_array($planttotal)) {
-    array_push($planttotalarray, $row['order_detail_unit_price']);
-}
 $plantpricearray = array();
-while ($row = mysqli_fetch_array($plantprice)) {
-    array_push($plantpricearray, $row['plant_price']);
-}
 $plantnamearray = array();
-while ($row = mysqli_fetch_array($plantname)) {
+
+while ($row = $plantstmt->fetch(PDO::FETCH_ASSOC)) {
+    array_push($plantquantityarray, $row['order_detail_qty']);
+    array_push($planttotalarray, $row['order_detail_unit_price']);
+    array_push($plantpricearray, $row['plant_price']);
     array_push($plantnamearray, $row['plant_name']);
 }
-$dateorderarray = array();
-while ($row = mysqli_fetch_array($dateorder)) {
-    array_push($dateorderarray, $row['order_date']);
-}
-
 
 $len = count($planttotalarray);
-$drop = $len+1;
-
+$drop = $len + 1;
 
 ?>
 
 <!-- your content in here -->
 <div class="admin">
-  <div class="admin__header">
-    <div class="admin__back">
-      <i class="ph-bold ph-arrow-left"></i>
-      <a href="./paid-transactions.php">Kembali</a>
+    <div class="admin__header">
+        <div class="admin__back">
+            <i class="ph-bold ph-arrow-left"></i>
+            <a href="./paid-transactions.php">Kembali</a>
+        </div>
+        <h1 class="admin__title">Paid transaction single</h1>
     </div>
-    <h1 class="admin__title">Paid transaction single</h1>
-  </div>
-  <div class="admin__body">
-    <div class="admin__card">
-      <?php
-      echo "<p>Tanggal transaksi: ".$dateorderarray[0]."</p>";
-      ?>
+    <div class="admin__body">
+        <div class="admin__card">
+            <?php
+            echo "<p>Tanggal transaksi: " . $dateorder . "</p>";
+            ?>
+        </div>
+        <div class="admin__card">
+            <table>
+                <?php
+                for ($rait = 0; $rait <= $drop; $rait++) {
+                    for ($don = 0; $don <= 4; $don++) {
+                        if ($rait == 0) {
+                            if ($don == 0) {
+                                echo "<thead><tr><th>No.</th>";
+                            } else if ($don == 1) {
+                                echo '<th>Plants</th>';
+                            } else if ($don == 2) {
+                                echo '<th>Price</th>';
+                            } else if ($don == 3) {
+                                echo '<th>Quantity</th>';
+                            } else {
+                                echo '<th>Sub-Total</th>';
+                            }
+                        } else if ($rait == $drop) {
+                            if ($don == 0) {
+                                echo '<tfoot><tr><td>total</td>';
+                            } else if ($don == 3) { // untuk total harga
+                                $quantity = array_sum($plantquantityarray);
+                                echo "<td>$quantity</td>";
+                            } else if ($don == 4) { // untuk total kuantitas
+                                $prices = array_sum($planttotalarray);
+                                echo "<td>$prices</td></tfoot>";
+                            } else {
+                                echo '<td> </td>';
+                            }
+                        } else if ($don == 0 && $rait != 0) {
+                            if ($rait != $drop) {
+                                if ($rait == 1) {
+                                    echo '<tbody><tr><td>' . $rait . '</td>';
+                                } else {
+                                    echo '<tr><td>' . $rait . '</td>';
+                                }
+                            }
+                        } else if ($don == 1 && $rait != 0 && $rait != $drop) { // kolom 1 untuk nama produk
+                            echo '<td>' . $plantnamearray[$rait - 1] . '</td>';
+                        } else if ($don == 2 && $rait != 0 && $rait != $drop) { // kolom 2 untuk harga produk
+                            echo '<td>' . $plantpricearray[$rait - 1] . '</td>';
+                        } else if ($don == 3 && $rait != 0 && $rait != $drop) { // kolom 3 untuk kuantitas produk
+                            echo '<td>' . $plantquantityarray[$rait - 1] . '</td>';
+                        } else {
+                            if ($rait != $len) { // kolom 4 untuk sub-total
+                                echo '<td>' . $planttotalarray[$rait - 1] . '</td>';
+                            } else if ($rait == $len) {
+                                echo '<td>' . $planttotalarray[$rait - 1] . '</td><tbody>';
+                            } else {
+                                echo '<td> </td>';
+                            }
+                        }
+                    }
+                }
+                ?>
+            </table>
+        </div>
     </div>
-    <div class="admin__card">
-<table>
-    <?php 
-    for ($rait = 0; $rait <= $drop; $rait++) 
-    {
-        for ($don = 0; $don <= 4; $don++) {
-            if ($rait == 0) {
-              if ($don == 0) {
-                  echo "<thead><tr><th>No.</th>";  
-                } else if ($don == 1) {
-                  echo '<th>Plants</th>';
-                } else if ($don == 2) {
-                  echo '<th>Price</th>';
-                } else if ($don == 3) {
-                  echo '<th>Quantity</th>';
-                } else {
-                  echo '<th>Sub-Total</th>';
-                }
-              } else if ($rait == $drop) {
-                if ($don == 0) {
-                  echo '<tfoot><tr><td>total</td>';
-                } else if ($don == 3){
-                  if ($plantquantitytotal){
-                    $row = mysqli_fetch_assoc($plantquantitytotal);
-                    $quantity = $row['quantity'];
-                    echo "<td>$quantity</td>";
-                  } else {
-                    echo 0;
-                  }
-                } else if($don == 4){
-                  if ($planttotalall){
-                    $row = mysqli_fetch_assoc($planttotalall);
-                    $prices = $row['prices'];
-                    echo "<td>$prices</td></tfoot>";
-                  } else {
-                    echo 0;
-                  }
-                } else {
-                  echo '<td> </td>';
-                }
-              } else if ($don == 0 && $rait != 0) {
-                if ($rait != $drop){
-                  if ($rait == 1) {
-                    echo '<tbody><tr><td>'.$rait.'</td>';
-                  } else {
-                    echo '<tr><td>'.$rait.'</td>';
-                  }
-                }
-              } else if ($don == 1 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$plantnamearray[$rait-1].'</td>';
-              } else if ($don == 2 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$plantpricearray[$rait-1].'</td>';
-              } else if ($don == 3 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$plantquantityarray[$rait-1].'</td>';
-              } else {
-                if ($rait != $len) {
-                  echo '<td>'.$planttotalarray[$rait - 1].'</td>';
-                } else if ($rait == $len) {
-                  echo '<td>'.$planttotalarray[$rait - 1].'</td><tbody>';
-                } else {
-                  echo '<td> </td>';
-                }
-            }
-        }
-    }
-    ?>
-</table>
-    </div>
-  </div>
 </div>
 <!-- end your content in here -->
 
--- manager/unpaid-transactions.php
+++ manager/unpaid-transactions.php
@@ -5,150 +5,170 @@ $title = 'Unpaid Transactions';
 
 require_once('layouts/header.php');
 
+// Database configuration
+$dbHost = 'localhost';
+$dbName = 'store';
+$dbUser = 'root';
+$dbPass = '';
+
+try {
+    $pdo = new PDO("mysql:host={$dbHost};dbname={$dbName}", $dbUser, $dbPass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+
 ?>
 
 <!-- your content in here -->
 <div class="admin">
-  <div class="admin__header">
-    <h1 class="admin__title">Unpaid transactions</h1>
-  </div>
-  <div class="admin__body">
-    <form action="./unpaid-transactions.php" method="get" class="admin__filters">
-      <input type="date" class="input" name="datestart"/>
-      <span>to</span>
-      <input type="date" class="input" name="dateend"/>
-      <button class="admin__button">Filter</button>
-    </form>
-    <div class="admin__card">
-      <h2 class="admin__card-title">Graph</h2>
-      <hr />
-      <canvas id="graph" class="graph"></canvas>
+    <div class="admin__header">
+        <h1 class="admin__title">Unpaid transactions</h1>
     </div>
-    <div class="admin__card">
-<table>
-    <?php 
-    $connect=mysqli_connect('localhost','root','','store');
-        
-    if (isset($_GET['datestart']) && isset($_GET['dateend'])) {
-      $datestart = $_GET['datestart'];
-      $dateend = $_GET['dateend'];
-    } else {
-      $datestart = '200-01-01';
-      $dateend = '5000-01-01';
-    }
+    <div class="admin__body">
+        <!-- filter tanggal -->
+        <form action="./paid-transactions.php" method="get" class="admin__filters"> 
+            <input type="date" class="input" name="datestart" />
+            <span>to</span>
+            <input type="date" class="input" name="dateend" />
+            <button class="admin__button">Filter</button>
+        </form>
+        <div class="admin__card">
+            <h2 class="admin__card-title">Graph</h2>
+            <hr />
+            <!-- grafik chart -->
+            <canvas id="graph" class="graph"></canvas>
+        </div>
+        <div class="admin__card">
+          <!-- tabel laporan -->
+            <table>
+                <?php
+                // Prepare statements untuk
+                $tanggalpenjualan = $pdo->prepare("SELECT order_date FROM orders WHERE order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend GROUP BY order_date");
+                $penjualan = $pdo->prepare("SELECT SUM(order_total_price) AS sold FROM orders WHERE order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend GROUP BY order_date");
+                $paymentmethod = $pdo->prepare("SELECT payment_method_bank FROM payment_methods paymet, orders ord WHERE paymet.payment_method_id = ord.payment_method_id AND order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend ORDER BY order_date");
+                $orderdate = $pdo->prepare("SELECT order_date FROM orders WHERE order_status = 'pending'AND order_date BETWEEN :datestart AND :dateend ORDER BY order_date");
+                $plantquantity = $pdo->prepare("SELECT order_detail_qty AS quantity FROM order_details ordet, orders ord WHERE ordet.order_id = ord.order_id AND ord.order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend ORDER BY ord.order_date");
+                $orderprice = $pdo->prepare("SELECT order_total_price FROM orders WHERE order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend ORDER BY order_date");
+                $linkid = $pdo->prepare("SELECT order_id FROM orders WHERE order_status = 'pending' AND order_date BETWEEN '1023-01-01' AND '5023-12-12' ORDER BY order_date");
 
-    $tanggalpenjualan = mysqli_query($connect, "SELECT order_date FROM orders WHERE order_status = 'Pending' AND order_date BETWEEN '$datestart' AND '$dateend' GROUP BY order_date");
-    $penjualan = mysqli_query($connect, "SELECT SUM(order_total_price) AS sold FROM orders WHERE order_status = 'Pending' AND order_date BETWEEN '$datestart' AND '$dateend' GROUP BY order_date");
+                $plantquantitytotal = $pdo->prepare("SELECT SUM(ordet.order_detail_qty) AS quantity FROM order_details ordet, orders ord WHERE ordet.order_id = ord.order_id AND ord.order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend");
+                $orderpricetotal = $pdo->prepare("SELECT SUM(order_total_price) AS prices FROM orders WHERE order_status = 'pending' AND order_date BETWEEN :datestart AND :dateend");
 
-    $paymentmethod = mysqli_query($connect, "SELECT payment_method_bank FROM payment_methods paymet, orders ord WHERE paymet.payment_method_id = ord.payment_method_id AND order_status = 'pending' AND order_date BETWEEN '$datestart' AND '$dateend' ORDER BY order_date");
-    $orderdate = mysqli_query($connect, "SELECT order_date FROM orders WHERE order_status = 'pending' AND order_date BETWEEN '$datestart' AND '$dateend' ORDER BY order_date");
-    $plantquantity = mysqli_query($connect, "SELECT order_detail_qty AS quantity FROM order_details ordet, orders ord WHERE ordet.order_id = ord.order_id AND ord.order_status = 'paid' ORDER BY ord.order_date");
-    $orderprice = mysqli_query($connect, "SELECT order_total_price FROM orders WHERE order_status = 'pending' AND order_date BETWEEN '$datestart' AND '$dateend' ORDER BY order_date");
-    $linkid = mysqli_query($connect, "SELECT order_id FROM orders WHERE order_status = 'pending' AND order_date BETWEEN '2000-01-01' AND '5000-12-12' ORDER BY order_date");
+                // Bind parameter
+                $datestart = isset($_GET['datestart']) ? $_GET['datestart'] : '200-01-01';
+                $dateend = isset($_GET['dateend']) ? $_GET['dateend'] : '5000-01-01';
 
-    $plantquantitytotal = mysqli_query($connect, "SELECT SUM(ordet.order_detail_qty) AS quantity FROM order_details ordet, orders ord WHERE ordet.order_id = ord.order_id AND ord.order_status = 'pending' AND order_date BETWEEN '$datestart' AND '$dateend'");
-    $orderpricetotal = mysqli_query($connect, "SELECT SUM(order_total_price) AS prices FROM orders WHERE order_status = 'pending' AND order_date BETWEEN '$datestart' AND '$dateend'");
+                $tanggalpenjualan->bindParam(':datestart', $datestart);
+                $tanggalpenjualan->bindParam(':dateend', $dateend);
+                $penjualan->bindParam(':datestart', $datestart);
+                $penjualan->bindParam(':dateend', $dateend);
+                $paymentmethod->bindParam(':datestart', $datestart);
+                $paymentmethod->bindParam(':dateend', $dateend);
+                $orderdate->bindParam(':datestart', $datestart);
+                $orderdate->bindParam(':dateend', $dateend);
+                $plantquantity->bindParam(':datestart', $datestart);
+                $plantquantity->bindParam(':dateend', $dateend);
+                $orderprice->bindParam(':datestart', $datestart);
+                $orderprice->bindParam(':dateend', $dateend);
+                //$linkid->bindParam(':datestart', $datestart);
+                //$linkid->bindParam(':dateend', $dateend);
+                $plantquantitytotal->bindParam(':datestart', $datestart);
+                $plantquantitytotal->bindParam(':dateend', $dateend);
+                $orderpricetotal->bindParam(':datestart', $datestart);
+                $orderpricetotal->bindParam(':dateend', $dateend);
 
-    $paymentmethodarray = array();
-    while ($row = mysqli_fetch_array($paymentmethod)) {
-        array_push($paymentmethodarray, $row['payment_method_bank']);
-    }
-    $orderdatearray = array();
-    while ($row = mysqli_fetch_array($orderdate)) {
-        array_push($orderdatearray, $row['order_date']);
-    }
-    $plantquantityarray = array();
-    while ($row = mysqli_fetch_array($plantquantity)) {
-        array_push($plantquantityarray, $row['quantity']);
-    }
-    $orderpricearray = array();
-    while ($row = mysqli_fetch_array($orderprice)) {
-        array_push($orderpricearray, $row['order_total_price']);
-    }
-    $linkidarray = array();
-    while ($row = mysqli_fetch_array($linkid)) {
-        array_push($linkidarray, $row['order_id']);
-    }
+                // Execute query
+                $tanggalpenjualan->execute();
+                $penjualan->execute();
+                $paymentmethod->execute();
+                $orderdate->execute();
+                $plantquantity->execute();
+                $orderprice->execute();
+                $linkid->execute();
+                $plantquantitytotal->execute();
+                $orderpricetotal->execute();
 
+                // Fetch data
+                $paymentmethodarray = $paymentmethod->fetchAll(PDO::FETCH_ASSOC);
+                $orderdatearray = $orderdate->fetchAll(PDO::FETCH_ASSOC);
+                $plantquantityarray = $plantquantity->fetchAll(PDO::FETCH_ASSOC);
+                $orderpricearray = $orderprice->fetchAll(PDO::FETCH_ASSOC);
+                $linkidarray = $linkid->fetchAll(PDO::FETCH_ASSOC);
 
-    $len = count($orderdatearray);
-    $drop = $len+1;
+                $len = count($orderdatearray);
+                $drop = $len + 1; //tinggi tabel sesuai banyak data + 1
 
-    for ($rait = 0; $rait <= $drop; $rait++) 
-    {
-        for ($don = 0; $don <= 5; $don++) {
-            if ($rait == 0) {
-              if ($don == 0) {
-                  echo "<thead><tr><th>No.</th>";  
-                } else if ($don == 1) {
-                  echo '<th>Payment Methods</th>';
-                } else if ($don == 2) {
-                  echo '<th>Date</th>';
-                } else if ($don == 3) {
-                  echo '<th>Total Items</th>';
-                } else if ($don == 4) {
-                  echo '<th>Total Price</th>';
-                } else {
-                  echo '<th> </th></tr></thead>';
+                for ($rait = 0; $rait <= $drop; $rait++) { //$don = kolom tabel, $rait = baris tabel
+                    for ($don = 0; $don <= 5; $don++) {
+                        if ($rait == 0) { //header tabel
+                            if ($don == 0) {
+                                echo "<thead><tr><th>No.</th>";
+                            } else if ($don == 1) {
+                                echo '<th>Payment Methods</th>';
+                            } else if ($don == 2) {
+                                echo '<th>Date</th>';
+                            } else if ($don == 3) {
+                                echo '<th>Total Items</th>';
+                            } else if ($don == 4) {
+                                echo '<th>Total Price</th>';
+                            } else {
+                                echo '<th> </th></tr></thead>';
+                            }
+                        } else if ($rait == $drop) {
+                            if ($don == 0) { //baris paling bawah
+                                echo '<tfoot><tr><td>total</td>';
+                            } else if ($don == 3) { //kolom 3 untuk total quantitas produk
+                                if ($plantquantitytotal) {
+                                    $quantity = $plantquantitytotal->fetchColumn();
+                                    echo "<td>$quantity</td>";
+                                } else {
+                                    echo 0;
+                                }
+                            } else if ($don == 4) { //kolom 4 untuk total harga produk
+                                if ($orderpricetotal) {
+                                    $prices = $orderpricetotal->fetchColumn();
+                                    echo "<td>$prices</td>";
+                                } else {
+                                    echo 0;
+                                }
+                            } else { //kosong, untuk tabel tanpa jumlah total
+                                echo '<td> </td>';
+                            }
+                        } else if ($don == 0 && $rait != 0) {
+                            if ($rait != $drop) { // isian nomor urut transaksi
+                                if ($rait == 1) {
+                                    echo '<tbody><tr><td>' . $rait . '</td>';
+                                } else {
+                                    echo '<tr><td>' . $rait . '</td>';
+                                }
+                            }
+                        } else if ($don == 1 && $rait != 0 && $rait != $drop) { // kolom 1 untuk metode pembayaran
+                            echo '<td>' . $paymentmethodarray[$rait - 1]['payment_method_bank'] . '</td>';
+                        } else if ($don == 2 && $rait != 0 && $rait != $drop) { // kolom 2 untuk tanggal
+                            echo '<td>' . $orderdatearray[$rait - 1]['order_date'] . '</td>';
+                        } else if ($don == 3 && $rait != 0 && $rait != $drop) { // kolom 3 untuk kuantitas produk
+                            echo '<td>' . $plantquantityarray[$rait - 1]['quantity'] . '</td>';
+                        } else if ($don == 5) {
+                            if ($rait == $len) { // kolom untuk info lebih lanjut
+                                echo '<td><a href="./unpaid-transaction-single.php?transid=' . $linkidarray[$rait - 1]['order_id'] . '">More Info</a></td></tbody>';
+                            } else {
+                                echo '<td><a href="./unpaid-transaction-single.php?transid=' . $linkidarray[$rait - 1]['order_id'] . '">More Info</a></td>';
+                            }
+                        } else {
+                            if ($rait != $drop) { // kolom 4 untuk harga 
+                                echo '<td>' . $orderpricearray[$rait - 1]['order_total_price'] . '</td>';
+                            } else {
+                                echo '<td> </td>';
+                            }
+                        }
+                    }
                 }
-              } else if ($rait == $drop) {
-                if ($don == 0) {
-                  echo '<tfoot><tr><td>total</td>';
-                } else if ($don == 3){
-                  if ($plantquantitytotal){
-                    $row = mysqli_fetch_assoc($plantquantitytotal);
-                    $quantity = $row['quantity'];
-                    echo "<td>$quantity</td>";
-                  } else {
-                    echo 0;
-                  }
-                } else if($don == 4){
-                  if ($orderpricetotal){
-                    $row = mysqli_fetch_assoc($orderpricetotal);
-                    $prices = $row['prices'];
-                    echo "<td>$prices</td>";
-                  } else {
-                    echo 0;
-                  }
-                } else if ($don == 6) {
-                  echo '<td> </td></tr></tfoot>';
-                } else {
-                  echo '<td> </td>';
-                }
-              } else if ($don == 0 && $rait != 0) {
-                if ($rait != $drop){
-                  if ($rait == 1) {
-                    echo '<tbody><tr><td>'.$rait.'</td>';
-                  } else {
-                    echo '<tr><td>'.$rait.'</td>';
-                  }
-                }
-              } else if ($don == 1 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$paymentmethodarray[$rait-1].'</td>';
-              } else if ($don == 2 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$orderdatearray[$rait-1].'</td>';
-              } else if ($don == 3 && $rait != 0 && $rait != $drop) {
-                echo '<td>'.$plantquantityarray[$rait-1].'</td>';
-              } else if ($don == 5) {
-                if ($rait == $len) {
-                  echo '<td><a href="./unpaid-transaction-single.php?transid='.$linkidarray[$rait-1].'">More Info</a></td></tbody>';
-                } else {
-                  echo '<td><a href="./unpaid-transaction-single.php?transid='.$linkidarray[$rait-1].'">More Info</a></td>';
-                }
-              } else {
-                if ($rait != $drop) {
-                  echo '<td>'.$orderpricearray[$rait - 1].'</td>';
-                } else {
-                  echo '<td> </td>';
-                }
-            }
-        }
-    }
-    ?>
-</table>
+                ?>
+            </table>
 
+        </div>
     </div>
-  </div>
 </div>
 <!-- end your content in here -->
 
@@ -157,32 +177,31 @@ require_once('layouts/header.php');
 
 <!-- custom js -->
 <script>
-  const ctx = document.getElementById('graph');
+    const ctx = document.getElementById('graph');
 
-  new Chart(ctx, {
-    type: 'bar',
-    data: {
-      labels: [
-        <?php while ($row = mysqli_fetch_array($tanggalpenjualan)){echo '"'.$row['order_date'].'",';} ?>
-      ], // Nama data yang dihitung
-      datasets: [{
-        label: '# of Transactions',
-        data: [
-          <?php while($row = mysqli_fetch_array($penjualan)){echo '"'.$row['sold'].'",';} ?>
-        ],  // Banyak data
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [
+                <?php while ($row = $tanggalpenjualan->fetch(PDO::FETCH_ASSOC)) {echo '"'.$row['order_date'].'",';} ?>
+            ], // Nama data yang dihitung
+            datasets: [{
+                label: '# of Transactions',
+                data: [
+                    <?php while ($row = $penjualan->fetch(PDO::FETCH_ASSOC)) {echo '"'.$row['sold'].'",';} ?>
+                ],  // Banyak data
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 </script>
-
 
 <?php
 
