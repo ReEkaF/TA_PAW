65689a9e3be32a7729c46b9863aed1b510201af8
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:13:59 +0700

    Update login.php

--- login.php
+++ login.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+// cek apakah cust sudah login
 if (isset($_SESSION['customer_id'])) {
   header("Location: ./products.php");
   exit();
@@ -10,21 +11,28 @@ if (isset($_SESSION['customer_id'])) {
 require_once('data/customer.php');
 require_once('libs/validation.php');
 
+// INSTANCE BOOLEAN UNTUK ERROR
 $login_error = null;
+// LIST TEMP ERROR MSG
 $errors = [];
+// LIST TEMP INPUTAN USER
 $old_inputs = [
   'email' => '',
 ];
-
+// JIKA TOMBOL MASUK (LOGIN) DIKLIK
 if (isset($_POST['submit'])) {
+  // DI VALIDASI
   validate_email($errors, $_POST, 'email');
   validate_password($errors, $_POST, 'password');
-
+  // JIKA TIDAK TERDAPAT ERROR VALIDASI 
   if (!$errors) {
+    // MENCARI DATA CUST 
     $customer = find_customer($_POST['email']);
-
+    // JIKA DITEMUKAN
     if ($customer) {
+      // CHECK PASSWORD CUST
       if (password_verify($_POST['password'], $customer['customer_password'])) {
+        // INSTANCE CUST_ID,CUST_EMAIL,CUST_PHOTO
         $_SESSION['customer_id'] = $customer['customer_id'];
         $_SESSION['customer_email'] = $customer['customer_email'];
         $_SESSION['customer_photo'] = $customer['customer_photo'];
@@ -32,13 +40,13 @@ if (isset($_POST['submit'])) {
         exit();
       }
     }
-
+    // JIKA TIDAK DITEMUKAN
     $login_error = 'Email atau password salah!';
   }
-
-  $old_inputs['email'] = htmlspecialchars($_POST['email']);
+  // SIMPAN INPUTAN USER
+  $old_inputs['email'] = $_POST['email'];
 }
-
+// HEADER
 $title = 'Login';
 require('layouts/header.php');
 
@@ -53,33 +61,44 @@ require('layouts/header.php');
   <div class="login container">
     <form action="./login.php" method="post" class="login__left">
       <h1>Login</h1>
+      <!-- ERROR MSG JIKA LOGIN GAGAL -->
       <?php if ($login_error != null) : ?>
         <div class="alert alert_danger">
           <?= $login_error; ?>
         </div>
       <?php endif; ?>
+      <!-- SUCCES MSG JIKA LOGIN BERHASIL -->
       <?php if (isset($_GET['success_message'])) : ?>
         <div class="alert alert_success">
           <?= $_GET['success_message']; ?>
         </div>
       <?php endif; ?>
+      <!-- INPUT EMAIL -->
       <div>
         <label for="email" class="input-label">Email <span class="text-danger">*</span></label>
         <input type="text" name="email" id="email" class="input" value="<?= $old_inputs['email'] ?>" />
+        <!-- ERR MSG EMAIL -->
         <?php if (isset($errors['email'])) : ?>
           <div class="input-error"><?= $errors['email'] ?></div>
         <?php endif; ?>
       </div>
+      <!-- INPUT PASSWORD -->
       <div>
         <label for="password" class="input-label">Password <span class="text-danger">*</span></label>
         <input type="password" name="password" id="password" class="input" />
         <div class="input-help">Harus berisi 8-16 karakter dengan minimal 1 huruf besar, 1 huruf kecil, 1 karakter spesial, dan tidak boleh mengandung spasi.</div>
+        <!-- ERR MSG PASSWORD -->
         <?php if (isset($errors['password'])) : ?>
           <div class="input-error"><?= $errors['password'] ?></div>
         <?php endif; ?>
       </div>
+      <!-- TOMBOL MASUK -->
       <button type="submit" name="submit" class="login__button">Masuk</button>
+
+      <!-- LINK REGISTER CUST -->
       <p>Belum punya akun? <a href="./register.php">Daftar</a></p>
+      
+      <!-- LINK LOGIN STAFF -->
       <p>
         <a href="./admin/login.php">Login Staff</a>
       </p>
@@ -93,7 +112,7 @@ require('layouts/header.php');
 <!-- end content -->
 
 <?php
-
+// FOOTER
 require('layouts/footer.php');
 
 ?>
