a5af5030146f4d9a26637700ec36c7fa5cd2e254
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:17:28 +0700

    Update transaction-edit-payment-method.php

--- transaction-edit-payment-method.php
+++ transaction-edit-payment-method.php
@@ -1,12 +1,12 @@
 <?php
 
 session_start();
-
+  // cek apakah cust sudah login
 if (!isset($_SESSION['customer_id'])) {
   header("Location: ./login.php");
   exit();
 }
-
+// JIKA TIDAK ADA ORDER YANG DIUBAH PEMBAYARANNYA ARAHKAN KE HAL ORDER
 if (!isset($_GET['order_id'])) {
   header("Location: ./transactions.php");
   exit();
@@ -15,30 +15,37 @@ if (!isset($_GET['order_id'])) {
 require_once('data/order.php');
 require_once('data/payment_method.php');
 
+// LIST TEMP ERROR 
 $errors = [];
+// AMBIL SELURUH ORDERAN CUST X 
 $order = find_order($_SESSION['customer_id'], $_GET['order_id']);
+// AMBIL SELURUH METHODE PEMBAYARAN
 $payment_methods = get_payment_methods();
 
+// JIKA TIDAK ADA ORDERAN
 if (!$order) {
   header("Location: ./transactions.php");
   exit();
 }
-
+// JIKA STATUS ORDERAN TELAH TERBAYAR
 if ($order['order_status'] == 'paid') {
   header("Location: ./transaction-single.php?order_id=" . $_GET['order_id']);
   exit();
 }
-
+// JIKA TOMBOL SIMPAN (SUBMIT) DIKLIK
 if (isset($_POST['submit'])) {
+  // JIKA METHODE PEMBAYARAN DIPILIH
   if (isset($_POST['payment_method_id'])) {
+    // UPDATE METHODE PEMBAYARAN
     update_order($_SESSION['customer_id'], $_GET['order_id'], $_POST['payment_method_id']);
     header("Location: ./transaction-single.php?order_id=" . $_GET['order_id']);
     exit();
   }
-
+  // ISI ERROR MSG
   $errors['payment_method_id'] = 'Pilih satu metode pembayaran.';
 }
 
+// HEADER
 $title = 'Ubah Metode Pembayaran Transaksi';
 require('layouts/header.php');
 
@@ -53,11 +60,14 @@ require('layouts/header.php');
   <div class="transaction-edit-method">
     <div class="transaction-edit-method__header">
       <p>
+        <!-- LINK KEMBALI -->
         <i class="ph-bold ph-arrow-left"></i>
         <a href="./transaction-single.php?order_id=<?= $_GET['order_id'] ?>">Kembali</a>
       </p>
+      <!-- H1 -->
       <h1>Ubah Metode Pembayaran #FF<?= $order['order_id'] ?></h1>
     </div>
+    <!-- FORM -->
     <form action="./transaction-edit-payment-method.php?order_id=<?= $_GET['order_id'] ?>" method="post" class="transaction-edit-method__body">
       <div class="transaction-edit-method__method">
         <div class="transaction-edit-method__method-group">
@@ -65,20 +75,22 @@ require('layouts/header.php');
             Metode Pembayaran <span class="text-danger">*</span>
           </h2>
           <div class="transaction-edit-method__method-list">
+            <!-- TAMPILKAN SEMUA METHODE PEMBAYARAN -->
             <?php foreach ($payment_methods as $payment_method) : ?>
-              <label>
+              <label class="<?= $payment_method['payment_method_id'] == $order['payment_method_id'] ? 'active' : '' ?>">
                 <input type="radio" name="payment_method_id" value="<?= $payment_method['payment_method_id'] ?>" <?= $payment_method['payment_method_id'] == $order['payment_method_id'] ? 'checked' : '' ?> />
-                <span></span>
                 <i class="ph-fill ph-check-circle"></i>
                 <img src="./assets/img/banks/<?= $payment_method['payment_method_logo'] ?>" alt="<?= $payment_method['payment_method_bank'] ?>" />
               </label>
             <?php endforeach; ?>
           </div>
+          <!-- JIKA TERDAPAT ERROR -->
           <?php if (isset($errors['payment_method_id'])) : ?>
             <div class="input-error"><?= $errors['payment_method_id'] ?></div>
           <?php endif; ?>
         </div>
       </div>
+      <!-- TOMBOL SIMPAN(SUBMIT) -->
       <button type="submit" name="submit" class="transaction-edit-method__button">
         Simpan
       </button>
@@ -87,6 +99,9 @@ require('layouts/header.php');
   <!-- end transaction edit payment method -->
 </main>
 <!-- end content -->
+
+<!-- js customs -->
+<script src="./assets/js/transaction-edit-payment-method.js"></script>
 
 <?php
 
