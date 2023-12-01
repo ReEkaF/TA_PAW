04834d9560a6d5648a51b4c70cad375296160955
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:11:21 +0700

    Update change-password.php

--- change-password.php
+++ change-password.php
@@ -1,36 +1,44 @@
 <?php
 
-error_reporting(E_ALL);
 session_start();
+require(__DIR__."/config/database.php");
+require(__DIR__."/libs/validation.php");
 
 if (!isset($_SESSION['customer_id'])) {
   header("Location: ./login.php");
   exit();
 }
-
-require_once("config/database.php");
-require_once("libs/validation.php");
-
+// KONEKSI DB
 $db = new PDO('mysql:host=localhost;dbname=' . DB_NAME, DB_USERNAME, DB_PASSWORD, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-$errors = [];
-
+// LIST TAMPUNG ARRAY ERROR
+$errors=[];
+// LIST TAMPUNG ARRAY JIKA BERHASIL UBAH PASSWORD
 $success = [];
-if (isset($_POST['ubahpass'])) {
-  validate_password($errors, $_POST, 'password');
-  if (!$errors) {
-    if ($_POST['password'] == $_POST['confirm_password']) {
-      $ubahpass = $db->prepare("UPDATE customers SET customer_password = :pwd WHERE customer_id = :custid ");
-      $ubahpass->bindValue(":pwd", password_hash(trim($_POST['password']), PASSWORD_DEFAULT));
-      $ubahpass->bindValue(":custid", $_SESSION['customer_id']);
+// JIKA USER MENGKLIK SIMPAN (UBAHPASS)
+if (isset($_POST['ubahpass'])){
+  // DIVALIDASI DAHULU APAKAH SESUAI FORMAT
+  validate_password($errors,$_POST,'password');
+  // JIKA SESUAI FORMAT MASUK IF DIBAWAH INI
+  if (!$errors){
+    // CEK APAKAH 2 INPUTAN PASSWORD BARU PENULISANNYA SAMA,YA-> UBAH PASSWORD
+    if ($_POST['password'] == $_POST['confirm_password']){
+      // UPDATE PASSWORD + HASHING
+      $ubahpass = $db->prepare("UPDATE customers set customer_password = :pwd where customer_id = :custid ");
+      $ubahpass->bindValue(":pwd",password_hash(trim($_POST['password']), PASSWORD_DEFAULT));
+      $ubahpass->bindValue(":custid",$_SESSION['customer_id']);
       $ubahpass->execute();
       $success['berhasil'] = "PASSWORD BERHASIL DIUBAH";
       $_POST['password'] = '';
-    } else {
+    }
+    // JIKA TIDAK SAMA
+    else{
       $errors['confirm_password'] = 'PASSWORD BERBEDA';
     }
   }
 }
 
+
+// HEADER
 $title = 'Ubah Password';
 require('layouts/header.php');
 
@@ -42,30 +50,38 @@ require('layouts/header.php');
 <!-- content -->
 <main>
   <!-- profile -->
+  <!-- TULISAN UBAH PASSWORD -->
   <div class="profile container">
     <div class="profile__header">
       <h1>Ubah Password</h1>
     </div>
     <div class="profile__body">
-
+      <!-- FORM (PASSWORD BARU, CONFIRM PASSWORD) -->
       <form action="./change-password.php" method="post" class="profile__right">
-        <?php if (isset($success['berhasil'])) {
+        <!-- JIKA SUDAH BERHASIL MENGUBAH PASSWORD -->
+        <?php if(isset($success['berhasil'])){
           echo "<div class='alert alert_success'> {$success['berhasil']} </div>";
-        } ?>
+        }?>
         <div class="profile__form">
           <div>
-
+            <!-- INPUT PASSWORD -->
             <label for="password" class="input-label">Password Baru <span class="text-danger">*</span></label>
-            <input type="password" id="password" name="password" class="input" value="<?= isset($_POST['password']) ? $_POST['password'] : ''  ?>" />
+            <input type="text" id="password" name="password" class="input" value="<?php if (isset($_POST['password'])){echo $_POST['password'];}  ?>"/>
+            <!-- FORMAT -->
             <div class="input-help">Harus berisi 8-16 karakter dengan minimal 1 huruf besar, 1 huruf kecil, 1 karakter spesial, dan tidak boleh mengandung spasi.</div>
-            <div class="input-error"><?= isset($errors['password']) ? $errors['password'] : '' ?></div>
+            <!-- ERR MESSAGE -->
+            <div class="input-error"><?php if(isset($errors['password'])){ echo $errors['password']; } ?></div>
           </div>
           <div>
+            <!-- INPUT CONFIRM PASSWORD -->
             <label for="confirm_password" class="input-label">Ketik Ulang Password <span class="text-danger">*</span></label>
-            <input type="password" id="confirm_password" name="confirm_password" class="input" />
-            <div class="input-error"><?= isset($errors['confirm_password']) ? $errors['confirm_password'] : '' ?></div>
+            <input type="text" id="confirm_password" name="confirm_password" class="input" />
+            <!-- ERR MESSAGE -->
+            <div class="input-error"><?php if(isset($errors['confirm_password'])){ echo $errors['confirm_password']; } ?></div>
+            <!-- <div class="input-error">Invalid confirm password.</div> -->
           </div>
           <div>
+            <!-- TOMBOL SIMPAN (UBAHPASS) -->
             <button type="submit" name="ubahpass" class="profile__button">Simpan</button>
           </div>
         </div>
@@ -77,7 +93,7 @@ require('layouts/header.php');
 <!-- end content -->
 
 <?php
-
+// FOOTER
 require('layouts/footer.php');
 
 ?>
