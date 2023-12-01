e7e2b0c46afca4f1253b0df7013d749135043b8f
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:15:09 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -1,23 +1,25 @@
+<link rel="stylesheet" href="./assets/css/profile.css">
 <?php
 
-session_start();
+  session_start();
 
-if (!isset($_SESSION['customer_id'])) {
-  header("Location: ./login.php");
-  exit();
-}
+  // cek apakah cust sudah login
+  if (!isset($_SESSION['customer_id'])) {
+    header("Location: ./login.php");
+    exit();
+  }
+  require_once('data/customer.php');
+  // AMBIL DATA CUST
+  $customer = find_customer_with_id($_SESSION['customer_id']);
 
-require_once('data/customer.php');
+  $title = 'Profil';
+  require('layouts/header.php');
 
-$customer = find_customer_with_id($_SESSION['customer_id']);
-
-$title = 'Profil Saya';
-require('layouts/header.php');
 
 ?>
 
 <!-- css customs -->
-<link rel="stylesheet" href="./assets/css/profile.css">
+
 
 <!-- content -->
 <main>
@@ -28,27 +30,33 @@ require('layouts/header.php');
     </div>
     <div class="profile__body">
       <div class="profile__left">
+        <!-- FOTO CUST -->
         <img src="<?= $customer['customer_photo'] == null ? './assets/img/default-profile.jpg' : './assets/img/profiles/' . $customer['customer_photo'] ?>" alt="<?= $customer['customer_name'] ?>" />
+
       </div>
-      <div class="profile__right">
         <div class="profile__form">
           <div>
-            <div class="input-label">Nama:</div>
-            <div class="input-label"><?= $customer['customer_name'] ?></div>
+            <!-- NAMA  -->
+            <a class="input-label">Nama</a>
+            <a class="input" ><?= $customer['customer_name'] ?></a>
           </div>
           <div>
-            <div class="input-label">Email:</div>
-            <div class="input-label"><?= $customer['customer_email'] ?></div>
+            <!-- EMAIL -->
+            <a class="input-label">Email</a>
+            <a class="input"><?= $customer['customer_email'] ?></a>
           </div>
           <div>
-            <div class="input-label">Telepon:</div>
-            <div class="input-label"><?= $customer['customer_phone'] ?></div>
+            <!-- NO TELEPHONE -->
+            <a class="input-label">Telepon</a>
+            <a class="input" ><?= $customer['customer_phone'] ?></a>
           </div>
-          <form action="./editprofile.php" method="get">
-            <button type="submit" class="profile__button">Ubah Data</button>
+          <!-- TOMBOL UBAH DATA (ubahprofil) -->
+          <form method="POST" action="editprofile.php">
+            <div>
+            <input type="submit" name="ubahprofil" value="UBAH DATA" class="profile__button">
+            </div>
           </form>
         </div>
-      </div>
     </div>
   </div>
   <!-- end profile -->
@@ -56,7 +64,7 @@ require('layouts/header.php');
 <!-- end content -->
 
 <?php
-
+// FOOTER
 require('layouts/footer.php');
 
 ?>
