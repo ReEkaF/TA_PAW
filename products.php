1335158dbf9505d4d75bfaa6412281819c27cc40
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:14:45 +0700

    Update products.php

--- products.php
+++ products.php
@@ -2,6 +2,7 @@
 
 session_start();
 
+// cek apakah cust sudah login
 if (!isset($_SESSION['customer_id'])) {
   header("Location: ./login.php");
   exit();
@@ -10,10 +11,14 @@ if (!isset($_SESSION['customer_id'])) {
 require_once('data/plant.php');
 require_once('data/category.php');
 
-$keyword = htmlspecialchars(isset($_GET['keyword']) ? $_GET['keyword'] : '');
+// KATA KUNCI
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
+// KATEGORI
 $category_id = isset($_GET['category_id']) ? $_GET['category_id'] : '';
 
+// AMBIL KATEGORI
 $categories = get_categories();
+// SEARCH ENGINE DENGAN KATAKUNCI & KATEGORI
 $plants = search_plants_with_category($keyword, $category_id, true);
 
 $title = 'Produk';
@@ -25,11 +30,20 @@ require('layouts/header.php');
 <main>
   <!-- filter -->
   <form action="./products.php" method="get" class="filter container">
+    <div class="filter__search">
+      <label for="keyword">
+        <i class="ph ph-magnifying-glass"></i>
+      </label>
+      <!-- INPUT KATA KUNCI -->
+      <input type="search" name="keyword" id="keyword" placeholder="Cari tanaman ..." value="<?= $keyword ?>" />
+      <button type="submit">Cari</button>
+    </div>
     <div class="filter__list">
       <div class="filter__item">
         <label for="category_id">
           <i class="ph ph-tag"></i>
         </label>
+        <!-- KATEGORI -->
         <select name="category_id" id="category_id">
           <option value="" selected>Semua</option>
           <?php foreach ($categories as $category) : ?>
@@ -38,26 +52,24 @@ require('layouts/header.php');
         </select>
       </div>
     </div>
-    <div class="filter__search">
-      <label for="keyword">
-        <i class="ph ph-magnifying-glass"></i>
-      </label>
-      <input type="search" name="keyword" id="keyword" placeholder="Cari tanaman ..." value="<?= $keyword ?>" />
-      <button type="submit">Cari</button>
-    </div>
   </form>
   <!-- end filter -->
   <!-- products -->
   <div class="products container">
     <div class="products__body">
+      <!-- TAMPILKAN HASIL PENCARIAN PRODUK -->
       <?php foreach ($plants as $plant) : ?>
         <div class="products__card">
           <div class="products__card-top">
+            <!-- GAMBAR PRODUK -->
             <img src="./assets/img/plants/<?= $plant['plant_photo'] ?>" alt="<?= $plant['plant_name'] ?>" class="products__card-img" />
           </div>
           <div class="products__card-body">
+            <!-- HARGA -->
             <p class="products__card-price">Rp<?= number_format($plant['plant_price']) ?></p>
+            <!-- NAMA -->
             <h3 class="products__card-name"><?= $plant['plant_name'] ?></h3>
+            <!-- KATEGORI, STOCK -->
             <p class="products__card-info">
               <i class="ph ph-tag"></i>
               <?= $plant['category_name'] ?>
@@ -65,12 +77,15 @@ require('layouts/header.php');
               <?= $plant['plant_stock'] ?> stok
             </p>
             <hr />
+            <!-- TOMBOL +KERANJANG & BELI SEKARANG -->
             <form action="./add-to-cart.php" method="post" class="products__card-actions">
+              <!-- +KERANJANG -->
               <input type="hidden" name="plant_id" value="<?= $plant['plant_id'] ?>">
               <button type="submit" name="add_to_cart" class="products__card-action-button">
                 <i class="ph ph-plus-circle"></i>
                 Keranjang
               </button>
+              <!-- BELI SEKARANG -->
               <button type="submit" name="buy_now" class="products__card-action-button products__card-action-button_primary">
                 <i class="ph ph-shopping-cart-simple"></i>
                 Beli Sekarang
@@ -79,6 +94,7 @@ require('layouts/header.php');
           </div>
         </div>
       <?php endforeach; ?>
+      <!-- JIKA HASIL PENCARIAN KOSONG -->
       <?php if (empty($plants)) : ?>
         <div class="products__empty">
           Produk kosong!
@@ -89,6 +105,9 @@ require('layouts/header.php');
   <!-- end products -->
 </main>
 <!-- end content -->
+
+<!-- js customs -->
+<script src="./assets/js/filter.js"></script>
 
 <?php
 
