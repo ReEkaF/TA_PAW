3acff9d4b8e2b88c2a57ab84a21ef214116bc31d
Graell29 <umuchtar0@gmail.com>
2023-12-01 07:46:55 +0700

    manager

--- admin/paid-transactions.php
+++ admin/paid-transactions.php
@@ -1,12 +1,5 @@
 <?php
 
-session_start();
-
-if (!isset($_SESSION['staff_id'])) {
-  header("Location: ./login.php");
-  exit();
-}
-
 $page = 'paid-transactions';
 $title = 'Transaksi Sudah Lunas';
 
