8de3a2a00538e9e1ef7e7b467e9797460000f4d6
blackhair8200 <151891634+blackhair8200@users.noreply.github.com>
2023-12-01 22:10:04 +0700

    Update add-to-cart.php

--- add-to-cart.php
+++ add-to-cart.php
@@ -1,30 +1,39 @@
 <?php
-
+// mulai sesi
 session_start();
 
+// cek apakah cust sudah login
 if (!isset($_SESSION['customer_id'])) {
   header("Location: ./login.php");
   exit();
 }
 
+// cek apakah mengklik tambah ke keranjang
 if (!isset($_POST['plant_id'])) {
   header("Location: ./products.php");
   exit();
 }
 
+// import func untuk CRUD informasi dari cart
 require_once('data/cart-item.php');
 
+// set cust id dan plant id
 $customer_id = $_SESSION['customer_id'];
 $plant_id = $_POST['plant_id'];
 
+// cek apakah tumbuhan tersebut sudah pernah ada di keranjang
 $cart_item = find_cart_item($customer_id, $plant_id);
 
+// jika tumbuhan tersebut belum pernah ditambahkan maka masukkan ke keranjang
 if (!$cart_item) {
   save_cart_item($customer_id, $plant_id);
-} else {
+} 
+// jika sudah pernah ditambahkan ke keranjang maka update nilainya + x
+else {
   update_cart_item($customer_id, $plant_id, $cart_item['cart_item_qty'] + 1);
 }
 
+// jika klik beli
 if (isset($_POST['buy_now'])) {
   header('Location: ./cart.php');
   exit();
